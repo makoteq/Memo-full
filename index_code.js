8979d5441e330fde5aaf647e5039f7f696a9498d
Makotek <makoteq@gmail.com>
2019-01-16 19:20:07 +0100

    no_matter

--- index_code.js
+++ index_code.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
+  if (window.matchMedia("(max-width: 500px)").matches) {
+    $(".fb-like").attr("data-size", "small");
+  }
   $("#button").click(function() {
     $(".tryb").slideToggle(200);
   });
-
   $.ajax({
     type: "POST",
     url: "tp_server.php",
-    success: function(result) {      
+    success: function(result) {
       var obj = JSON.parse(result);
-      var txt="<tr><th>User</th><th>Complete Levels</th><th>Mode</th></tr>";
-      for(i=0;i<3;i++){
-        txt+="<tr><td>"+obj.results[i].username+"</td>";
-        txt+="<td>"+obj.results[i].level+"</td>";
-        txt+="<td>"+obj.results[i].Mode+"</td></tr>";
+      var txt = "<tr><th>User</th><th>Complete Levels</th><th>Mode</th></tr>";
+      for (i = 0; i < 3; i++) {
+        txt += "<tr><td>" + obj.results[i].username + "</td>";
+        txt += "<td>" + obj.results[i].level + "</td>";
+        txt += "<td>" + obj.results[i].Mode + "</td></tr>";
       }
       $("#list").html(txt);
     },
@@ -21,7 +23,4 @@ $(document).ready(function() {
       alert(thrownError);
     }
   });
-
 });
-
-  
